4dc7e6d887bfab31426a632f4c02c5906f33f302
RahulSitaram0013 <rahul.sitaram0013@gmail.com>
2024-12-01 03:13:59 +0530

    Access Not Confirmed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './Heart Prediction/App'
-
+// ConvertApppkToApp
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Apppk />
   </StrictMode>,
 )
